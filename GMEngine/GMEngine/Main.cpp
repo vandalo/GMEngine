f3e5d3c106f545cfdf0a9a082cb0cc8c9e1a88e9
ferran coma rosell <ferrancoma@hotmail.com>
2018-12-03 21:06:04 +0100

    create and configure project

---
+++ GMEngine/GMEngine/Main.cpp
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+#include "SDL/include/SDL.h"
+
+#pragma comment( lib, "SDL/libx86/SDL2.lib" )
+#pragma comment( lib, "SDL/libx86/SDL2main.lib" )
+
+int main(int argc, char ** argv)
+{
+	return 0;
+}
